8fce26377105daa273295324d518c852f3798d58
rhr18818 <rhr14101@gmail.com>
2025-04-19 20:10:28 +0600

    UnCommenting --part2.py

--- part2.py
+++ part2.py
@@ -1,26 +1,26 @@
-# import cv2
+import cv2
 
-# face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-# while True:
+while True:
 
-#     success, img = cap.read()
-#     if not success:
-#         print("Failed to grab frame")
-#         break
+    success, img = cap.read()
+    if not success:
+        print("Failed to grab frame")
+        break
 
-#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-#     for (x, y, w, h) in faces:
-#         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-#     cv2.imshow('img', img)
+    cv2.imshow('img', img)
 
-#     if cv2.waitKey(30) & 0xff == 27:  # Press ESC to exit
-#         break
+    if cv2.waitKey(30) & 0xff == 27:  # Press ESC to exit
+        break
 
-# cap.release()
-# cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
